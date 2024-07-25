ed260d179c062336c1142bf0dc2b0abb344ccac8
AlexRoomly <roomly2@gmail.com>
2024-07-25 13:20:08 +0300

    Add files via upload

---
+++ module_13_4.py
@@ -0,0 +1,49 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+import asyncio
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(text='Calories')
+async def set_age(message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=float(message.text))
+    await message.answer('Введите свой рост (см.):')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=float(message.text))
+    await message.answer('Введите свой вес (кг.):')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=float(message.text))
+    data = await state.get_data()
+    norm_cal = (10.0 * data['weight']) + (6.25 * data['growth']) - (5.0 * data['age']) + 5.0
+    await message.answer(f'Ваша норма калорий, необходимая для '
+                         f'функцйонирования организма - {norm_cal} калорий.')
+    await state.finish()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
